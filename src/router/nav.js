a9af88d423f0720697dd3065a5be9b698bdebe0f
yoona <1347499464@qq.com>
2021-02-05 20:59:55 +0800

    更新路由

--- src/router/nav.js
+++ src/router/nav.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, NavigationState } from '@react-navigation/native';
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from '@react-navigation/stack';
 
 // 路由
 
@@ -30,12 +33,42 @@ class Nav extends React.Component {
   }
   render() {
     return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home" headerMode={'none'}>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Test" component={Test} />
+      <NavigationContainer
+        onStateChange={(state: NavigationState) => {
+          //加入该行
+          //这个是跳转了才去回调，所以不能利用routes来判断路由栈
+          NavigationHelper.navRouters = state.routes;
+        }}
+      >
+        <Stack.Navigator
+          initialRouteName="Home"
+          headerMode={'none'}
+          screenOptions={(navigation) => {
+            NavigationHelper.navigation = navigation.navigation;
+          }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              ...TransitionPresets.SlideFromRightIOS,
+            }}
+          />
+          <Stack.Screen
+            name="Test"
+            component={Test}
+            options={{
+              ...TransitionPresets.SlideFromRightIOS,
+            }}
+          />
           {/*<Stack.Screen name="Tab" component={Tab} />*/}
-          <Stack.Screen name="ImageShow" component={ImageShow} />
+          <Stack.Screen
+            name="ImageShow"
+            component={ImageShow}
+            options={{
+              ...TransitionPresets.SlideFromRightIOS,
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     );
